fa3badfa5cc6fa79689c62650ae0f16b504724b5
carlosulker <carlosjunio-cj@hotmail.com>
2025-10-21 20:54:43 -0300

    add Footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function footer() {
+    return (
+    <footer className="bg-gray-800 text-white">
+        <div className="container mx-auto px-6 py-3">
+            <p className="text-center">
+                Todos direitos reservados
+            </p>
+        </div>
+    </footer>    
+    )
+}
