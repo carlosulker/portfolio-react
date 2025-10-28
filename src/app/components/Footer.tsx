48b422f249995cfcdcea98d5f0b87c8731efcab7
carlosulker <carlosjunio-cj@hotmail.com>
2025-10-27 21:53:43 -0300

    change main page and fix header/footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function footer() {
     return (
-    <footer className="bg-gray-800 text-white">
+    <footer className="sticky bottom-0 bg-background border-t shadow-sm">
         <div className="container mx-auto px-6 py-3">
             <p className="text-center">
                 Todos direitos reservados
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>  
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-ceter">
+      <section className="flex flex-col items-center justify-center w-full flex-1 px-20 text-ceter">
         <h1 className="text-6xl">Bem Vindo ao meu <span className="text-blue-600">Portfólio!</span> </h1>
         <p className="mt-3 text-2xl">
           Comece a construir o seu portfólio profissional hoje!
@@ -18,7 +18,20 @@ export default function Home() {
           <p className="mt-4 text-xl">Gerencie seu portfólio (requer login).</p>
         </Link>
       </div>
-      </main>
+      </section>
+      <section>
+        <div className="w-full px-20 bg-secondary">
+          <h2 className="text-4xl font-bold mb-4">Sobre o Projeto</h2>
+          <p className="text-xl mb-4">
+            Sou uma pessoa apaixonada por criar, aprender e transformar ideias em resultados concretos. Tenho experiência em
+             Analista de TI e busco sempre unir técnica e criatividade para entregar projetos com propósito e impacto.
+          </p>
+          <p className="text-xl">
+            Cada projeto aqui conta uma parte da minha jornada — ideias que se tornaram realidade, desafios que inspiraram 
+            soluções e criações que refletem minha forma de ver o mundo. Explore, descubra e conheça mais sobre o meu processo e minhas inspirações.
+          </p>
+        </div>
+      </section>
     </div>
     </>
   );
