33e66c32bff759a760ec0b88c3d9c8688a46045a
carlosulker <carlosjunio-cj@hotmail.com>
2025-12-15 22:44:07 -0300

    add Page to create a new Portfolio Project

--- src/app/components/PortfolioForm.tsx
+++ src/app/components/PortfolioForm.tsx
@@ -2,11 +2,17 @@
 
 import {z} from "zod"
 import { Button } from "@/components/ui/button";
-import { FormControl, FormDescription, FormField, FormItem, FormLabel, Form, FormMessage } from "@/components/ui/form";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, Form, FormMessage,    
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
+const MAX_FILE_SIZE = 5000000 // 5MB
 
 const formSchema = z.object({
     title: z.string().min(2, 
@@ -15,31 +21,55 @@ const formSchema = z.object({
             {message: "O título deve ter no máximo 50 caracteres"}
         ),
     description: z.string(),
-    imageUrl: z.string().url({message: "A URL da imagem é inválida"}),
+    image: z.any()
+        .refine((files) => files?.length == 1, {message: "A imagem é obrigatória"})
+        .refine((files) => files?.[0].size <= MAX_FILE_SIZE, {message: "A imagem deve ter no máximo 5MB"}),
     tags: z.string(),
-    projectUrl: z.string().url({message: "A URL do projeto é inválido"}),
-    githubUrl: z.string().url({message: "A URL do github é inválido"})
+    projectUrl: z.string().optional().refine(
+            (url) => !url || z.string().url().safeParse(url).success, 
+            { message:"URL inválida"}
+    ),
+    githubUrl: z.string().optional().refine(
+            (url) => !url || z.string().url().safeParse(url).success, 
+            { message:"URL inválida"}
+),
 })
 
 export default function portfolioForm() {
+    const [previewImage, setPreviewImage] = useState<string | null>(null)
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             title:"",
             description:"",
-            imageUrl:"",
             tags:"",
             projectUrl:"",
             githubUrl:""
         }
     });
 
+    const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        if (!file) return
+    
+        const reader = new FileReader()
+        
+        reader.onloadend = () => {
+            setPreviewImage(reader.result as string)
+        }  
+        
+        reader.readAsDataURL(file)
+    }
+
     const handleFormSubmit = (value: z.infer<typeof formSchema>) => {
         console.log("Formulário enviado", value)
     }
 
     return (
-        <Form {...form}>
+        <div className="flex flex-col md:flex-row gap-6">
+            <Card className="w-full md:w-1/2 px-8 py-4">
+            <Form {...form}>
             <form className="space-y-8" onSubmit={form.handleSubmit(handleFormSubmit)}>    
                 <FormField
                  control={form.control}
@@ -71,12 +101,16 @@ export default function portfolioForm() {
             />
             <FormField
                  control={form.control}
-                 name="imageUrl" 
-                 render={({field}) => (
+                 name="image" 
+                 render={() => (
                     <FormItem>
                         <FormLabel>URL da Imagem</FormLabel>
                         <FormControl>
-                            <Input {...field} />
+                            <Input 
+                                type="file"
+                                accept="image/*"
+                                onChange={handleImageUpload}
+                            />
                         </FormControl>
                         <FormDescription>
                          a URL da imgem principal do seu projeto
@@ -134,5 +168,29 @@ export default function portfolioForm() {
             <Button type="submit">Salvar</Button>
         </form>
     </Form>
+    </Card>
+    <Card className="w-full  md:w-1/2">
+                <CardHeader>
+                    <CardTitle>Preview da Imagem</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    {previewImage ? (
+                        <div className="relative h-64">
+                            <Image 
+                                alt="Preview da Imagem do projeto"
+                                src={previewImage}
+                                layout="fill"
+                                objectFit="cover"
+                                className="rouded-md"
+                            />
+                        </div>
+                    ) : (
+                        <div className="w-full h-64 flex items-center justify-center rounded-md">
+                            <p>Nenhuma imagem selecionada</p>
+                        </div>
+                    )}
+                </CardContent>
+    </Card>
+        </div>
     );
 }
